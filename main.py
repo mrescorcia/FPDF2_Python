13f34c4e8bb8931ef405048b842a9e10d8bdf651
David Escorcia Gomez <escorciagomezdavid@gmail.com>
2022-04-12 20:52:58 -0500

    added gitignore with venv

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def run():
     pdf.multi_cell(0,5,"Habia un dia: En el documento de entga de proyecto."+
     "El decia mucho eso. El historial de avance. Para ver esa linea de tiempo."+ 
     "Para tener la experiencia en Campo.")
+    
     pdf.output("tuto1.pdf")
 
 if __name__ == '__main__':
